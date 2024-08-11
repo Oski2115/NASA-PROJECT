ac5e8322144d69d009c6a3e6edd52d083631af56
Oskar Basiaga <basiagaoskar@gmail.com>
2024-08-11 13:37:06 +0200

    delete

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -7,13 +7,17 @@ const launch = {
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
     launchDate: new Date('December 25, 2030'),
-    destination: 'Kepler-442 b',
+    target: 'Kepler-442 b',
     customer: ['NASA', 'Slime'],
     upcoming: true,
     success: true,
 };
 
 launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+    return launches.has(launchId);
+}
 
 function getAllLaunches() {
     return Array.from(launches.values());
@@ -30,7 +34,17 @@ function addNewLaunch(launch) {
 
 }
 
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
+}
+
+
 module.exports = {
+    existsLaunchWithId,
     getAllLaunches,
     addNewLaunch,
+    abortLaunchById,
 }
